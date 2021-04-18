2c702dec0d6b8b1154fb70dd5524144d377f6860
David <david-adeniji@hotmail.co.uk>
2021-04-18 12:43:18 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -457,7 +457,7 @@ def event_b(colour):
 
 # Run app and display result inline in the notebook
 if __name__ == '__main__':
-    app.run_server(debug=True, threaded=True, use_reloader=False) 
+    app.run_server() 
 
 
 
